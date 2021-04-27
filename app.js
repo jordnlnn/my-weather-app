13e6a32e116683abbe51201f29fd64ca62662890
jordnlnn <80535966+jordnlnn@users.noreply.github.com>
2021-04-27 08:26:59 -0700

    updated main weather icon

--- app.js
+++ app.js
@@ -25,47 +25,76 @@ let dateElement = document.querySelector("#date-text");
 let now = new Date();
 dateElement.innerHTML = displayDateTime(now);
 
-//current temp display
+//my weather stats
 let apiKey = "974867647183f192d10e0478c4341263";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial";
+let myCity = "Phoenix";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${myCity}&units=imperial`;
 
-function showMyTemperature(response) {
+function showMyData(response) {
   let temperature = Math.round(response.data.main.temp);
   let mainTemp = document.querySelector("#main-temp");
   mainTemp.innerHTML = `${temperature}°F`;
-}
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showMyTemperature);
 
-//my weather description
-let descriptionURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=974867647183f192d10e0478c4341263";
-function showDescription(response) {
-  let description = response.data.weather[0].main;
+  let description = response.data.weather[0].description;
   let weatherDescription = document.querySelector("#weather-description");
   weatherDescription.innerHTML = description;
+
+  let humidity = response.data.main.humidity;
+  let myHumidity = document.querySelector("#humidity");
+  myHumidity.innerHTML = `Humidity: ${humidity}%`;
+
+  let wind = Math.round(response.data.wind.speed);
+  let myWind = document.querySelector("#wind");
+  myWind.innerHTML = `Wind: ${wind}mph`;
+
+  let myIcon = document.querySelector("img");
+  myIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
-axios.get(descriptionURL).then(showDescription);
+console.log(`${apiUrl}&appid=${apiKey}`);
+axios.get(`${apiUrl}&appid=${apiKey}`).then(showMyData);
 
 //search box form
 function searchForCity(event) {
   event.preventDefault();
-  let input = document.querySelector("#search-input").value;
+  let cityInput = document.querySelector("#search-input").value;
   let apiKey = "974867647183f192d10e0478c4341263";
   let units = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=${units}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=${units}&appid=${apiKey}`;
   axios.get(`${apiUrl}`).then(displayWeatherData);
 }
 //display weather update
 function displayWeatherData(response) {
-  let searchedCity = document.querySelector("#city-text");
-  let currentTemp = document.querySelector("#main-temp");
+  let updatedTemp = document.querySelector("#main-temp");
   let temperature = Math.round(response.data.main.temp);
-  currentTemp.innerHTML = `${temperature}°F`;
+  updatedTemp.innerHTML = `${temperature}°F`;
+
+  let searchedCity = document.querySelector("#city-text");
   let city = response.data.name;
   searchedCity.innerHTML = `${city.trim()}`;
+
+  let description = response.data.weather[0].description;
+  let updatedDescription = document.querySelector("#weather-description");
+  updatedDescription.innerHTML = description;
+
+  let humidity = response.data.main.humidity;
+  let updatedHumidity = document.querySelector("#humidity");
+  updatedHumidity.innerHTML = `Humidity: ${humidity}%`;
+
+  let wind = Math.round(response.data.wind.speed);
+  let updatedWind = document.querySelector("#wind");
+  updatedWind.innerHTML = `Wind: ${wind}mph`;
+
+  let updatedIcon = document.querySelector("img");
+  updatedIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
   let dateElement = document.querySelector("#date-text");
-  dateElement.innerHTML = displayDateTime(response.data.dt * 1000);
+  dateElement.innerHTML = displayDateTime(now);
 }
 //search button submission
 let submitCity = document.querySelector("#search-box-form");
